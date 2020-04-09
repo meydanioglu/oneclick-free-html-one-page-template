249d324e8616964c567027b1d21016c5f68d8a7b
Aydın Meydanioğlu <aydinmeydanioglu@yandex.com>
2020-04-10 01:27:36 +0300

    d
    tüm dosyalar

---
+++ src/scripts/index.js
@@ -0,0 +1,10 @@
+import '../styles/index.scss';
+
+console.log('webpack starterkit');
+
+$(document).ready(function() {
+    //carousel options
+    $('#quote-carousel').carousel({
+      pause: true, interval: 10000,
+    });
+  });
